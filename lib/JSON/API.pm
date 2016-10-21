1a8facb574f9f0c431bba239edcd2f020968501b
Timothe Litt <tlhackque@cpan.org>
2016-10-21 12:06:55 -0400

    Use a vstring for version so use JSON::API 1.1.0 works.
    The value of $VERSION must be a vstring for 3-digit versions to work with 'use module version'.

--- lib/JSON/API.pm
+++ lib/JSON/API.pm
@@ -9,7 +9,7 @@ use URI::Encode qw/uri_encode/;
 BEGIN {
 	use Exporter ();
 	use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
-	$VERSION     = '1.1.0';
+	$VERSION     = v1.1.0;
 	@ISA         = qw(Exporter);
 	#Give a hoot don't pollute, do not export more than needed by default
 	@EXPORT      = qw();
